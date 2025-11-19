d56ea94a63f7c0caa7f0a414197ed4cfe0e98e1d
Carlos Martorell <cmartorellotal@gmail.com>
2025-11-19 17:28:14 +0100

    feat(config): Dynamic PORT and environment variables setup for Render

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
 
 
   app.enableCors({
-    origin: 'http://localhost:4200', 
+    origin: [
+      'http://localhost:4200',           
+      'https://tu-frontend.vercel.app'   
+    ], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
